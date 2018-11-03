e67331c6d6d76c8f87d895274392b3d493ec4de1
ch2010 <44719394+ch2010@users.noreply.github.com>
2018-11-03 15:58:16 -0300

    Create olhos.py

---
+++ olhos.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+
+#Biblioteca de deteccao da Face
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#Biblioteca de deteccao dos olhos
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0) 
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces: #Contorna a face
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes: #Contorna os  olhos
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    cv2.imshow('Face',img) #Exibe a imagem
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
