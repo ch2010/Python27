f259ae9ec265deb6553b92e5e68fbb12534227f5
Carlos Rocha <44719394+ch2010@users.noreply.github.com>
2018-11-26 09:23:22 -0200

    Compara e Encontra  imagens  de acordo com a  calibragem

---
+++ Compara/pecas.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img_rgb = cv2.imread('carro.jpg')
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+template = cv2.imread('capture.jpg',0)
+w, h = template.shape[::-1]
+
+
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+threshold = 0.8 # Calibragem fina da busca
+loc = np.where( res >= threshold)
+
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+
+cv2.imshow('Detected',img_rgb)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#Carlos Rocha 
